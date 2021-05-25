8b7cbff3474a7672870f29a6edfd27de48476f95
Dharmi Kumbhani <dharmikumbhani201@gmail.com>
2021-05-25 21:04:25 +0530

    feat(data.js):Added a few dummy data

---
+++ data.js
@@ -0,0 +1,26 @@
+export const slangs = [
+    {
+        slang: 'FUD',
+        fullForm: 'Fear Uncertainity Doubt',
+        emoji: '',
+        description: 'Used when there is a bearish feel to the market and a lot of negative news coming in for the cryptocurrency.'
+    },
+    {
+        slang: 'HODL',
+        fullForm: 'Hold On for Dear Life',
+        emoji: '',
+        description: 'Advising/Informing not selling the particular crypto in context or all cryptos in general.'
+    },
+    {
+        slang: 'DYOR',
+        fullForm: 'Do your own research',
+        emoji: '',
+        description: 'Dont make investments on the basis of only someone elses say, do your own research!'
+    },
+    {
+        slang: 'DYOR',
+        fullForm: 'Do your own research',
+        emoji: '',
+        description: 'Dont make investments on the basis of only someone elses say, do your own research!'
+    }
+]
